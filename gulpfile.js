1b907e8130e5f1c100d8bde0597552c37bc6a7e4
Gaël Poupard <gael.poupard@orange.com>
2020-08-19 18:42:07 +0200

    feat(PW-2020)[WIP]: Introduction slides…

--- gulpfile.js
+++ gulpfile.js
@@ -9,36 +9,31 @@ const rename   = require('gulp-rename');
 const browser  = require('browser-sync').create();
 const newer    = require('gulp-newer');
 
-let paths = {
-    dev: './accesslide/',
-    node: './node_modules/'
-};
-
 /**
  * @section Build
  * Compile JS & CSS files for theme
  */
  
 gulp.task('css', function () {
-    return gulp.src(paths.dev + '/scss/*.scss')
-      .pipe(newer(paths.dev + '/css'))
+    return gulp.src('./accesslide/scss/*.scss')
+      .pipe(newer('./accesslide/css'))
       .pipe(maps.init())
       .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
       .pipe(prefix())
       .pipe(maps.write('./'))
-      .pipe(gulp.dest(paths.dev + '/css'))
+      .pipe(gulp.dest('./accesslide/css'))
       .pipe(browser.stream());
 });
 
 gulp.task('js', function () {
-    return gulp.src(paths.dev + '/*.js')
-      .pipe(newer(paths.dev + '/js'))
+    return gulp.src('./accesslide/*.js')
+      .pipe(newer('./accesslide/js'))
       .pipe(uglify().on('error', function(err) {
         console.error(`${err.cause.message} in ${err.cause.filename} at line ${err.cause.line}`);
         this.emit('end');
       }))
       .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest(paths.dev + '/js'))
+      .pipe(gulp.dest('./accesslide/js'))
       .pipe(browser.stream());
 });
 
@@ -67,9 +62,8 @@ function sync(done) {
 
 function watch() {
     gulp.watch('./**/*.html', gulp.series(reload));
-    gulp.watch(paths.dev + '/scss/**/*.scss', gulp.series('css', reload));
-    gulp.watch(paths.dev + '/*.js', gulp.series('js', reload));
+    gulp.watch('./accesslide/scss/**/*.scss', gulp.series('css', reload));
+    gulp.watch('./accesslide/*.js', gulp.series('js', reload));
 }
 
-exports.watch   = watch;
 exports.default = gulp.series( 'build', sync, watch );
